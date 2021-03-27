643277b58bd8212169f6dea995419c7e9c2ed571
ashns <35615496+ashns@users.noreply.github.com>
2021-03-27 13:35:33 -0400

    made actions for coffee and donut buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,41 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
-//test//
+    @FXML
+    Button donutBTN = new Button();
+    @FXML
+    Button coffeeBTN = new Button();
+
+    public void pressDonut(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("donut.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pressCoffee(ActionEvent event) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("coffee.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
