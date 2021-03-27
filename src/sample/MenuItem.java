4b9ad1d0f7db4a89385d70651a2f1db2f550c27b
mbw84 <mbw84@scarletmail.rutgers.edu>
2021-03-27 14:12:47 -0400

    Update MenuItem.java
    Now coded

--- src/sample/MenuItem.java
+++ src/sample/MenuItem.java
@@ -1,4 +1,17 @@
 package sample;
 
 public class MenuItem {
+    double itemPrice;
+
+    public MenuItem(){
+        itemPrice = 0;
+    }
+
+    void setPrice(double newPrice){
+        itemPrice = newPrice;
+    }
+
+    double getPrice(){
+        return itemPrice;
+    }
 }
