487a213488b365f6232a4695c64a3397ca31a2a6
ashns <35615496+ashns@users.noreply.github.com>
2021-03-27 14:51:24 -0400

    coffeeAddOns

---
+++ src/sample/coffeeAddOns.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class coffeeAddOns extends MenuItem{
+    final int CREAM = 1;
+    final int SYRUP = 2;
+    final int MILK = 3;
+    final int CARAMEL = 4;
+    final int WHIPPED_CREME = 5;
+    int type;
+    public coffeeAddOns(double price, int number, int type) {
+        super(price, number);
+        this.type = type;
+    }
+}
