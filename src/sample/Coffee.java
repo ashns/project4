e99894ce27025abdfdaf18d5ea5e4bf709917eee
ashns <35615496+ashns@users.noreply.github.com>
2021-03-27 14:36:29 -0400

    worked on basis for coffee.java

--- src/sample/Coffee.java
+++ src/sample/Coffee.java
@@ -1,4 +1,39 @@
 package sample;
 
-public class Coffee {
+public class Coffee extends MenuItem implements Customizable{
+
+    final int SIZE_SHORT = 1;
+    final int SIZE_TALL = 2;
+    final int SIZE_GRANDE = 3;
+    final int SIZE_VENTI = 4;
+    final double SHORT_PRICE = 1.99;
+    final double TALL_PRICE = 2.49;
+    final double GRANDE_PRICE = 2.99;
+    final double VENTI_PRICE = 3.49;
+    final double ADDON_COST = .20;
+
+    int size;
+    int quantity;
+    String addOns[] = new String[5];
+
+    public Coffee(double price, int size, int quantity, String addOns[]) {
+        super(price);
+        this.size = size;
+        this.addOns=addOns;
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object obj) {
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
 }
