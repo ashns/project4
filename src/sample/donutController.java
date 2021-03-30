3dd761f679af1d2686f0ca73663586c853582ee8
ashns <35615496+ashns@users.noreply.github.com>
2021-03-30 17:28:00 -0400

    worked on donut controller

--- src/sample/donutController.java
+++ src/sample/donutController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -18,11 +19,33 @@ public class donutController {
     @FXML
     public ListView currentListView;
     Controller main = new Controller();
+    final int YEAST_DONUT = 1;
+    final int CAKE_DONUT = 2;
+    final int DONUT_HOLE = 3;
 
 
     public void setMainController(Controller parent){
         main = parent;
     }
 
+    public int getType(){
+        String type = (String)donutTypeCB.getSelectionModel().getSelectedItem();
+        if(type=="yeast"){
+            return YEAST_DONUT;
+        }
+        else if(type=="cake"){
+            return CAKE_DONUT;
+        }
+        else{
+            return DONUT_HOLE;
+        }
+    }
 
+    public void pressOrderBTN(ActionEvent actionEvent) {
+        String flavor = (String)donutFlavorCB.getSelectionModel().getSelectedItem();
+        int type = getType();
+        int quantity = (int)quantitySlider.getValue();
+        Donut newDonut = new Donut(quantity, flavor, type);
+
+    }
 }
