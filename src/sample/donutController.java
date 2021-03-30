9eeaeda298a0d6fb393a910dd0ce0a3bc6bc2c72
ashns <35615496+ashns@users.noreply.github.com>
2021-03-30 17:18:54 -0400

    worked on alot

--- src/sample/donutController.java
+++ src/sample/donutController.java
@@ -1,9 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+
 public class donutController {
+    @FXML
+    public ComboBox donutTypeCB;
+    @FXML
+    public ComboBox donutFlavorCB;
+    @FXML
+    public Button orderBTN;
+    @FXML
+    public Slider quantitySlider;
+    @FXML
+    public ListView currentListView;
     Controller main = new Controller();
+
+
     public void setMainController(Controller parent){
         main = parent;
-
     }
+
+
 }
