fdd98a71bdd8cb471a48e25ad7ea81c1e3553086
mbw84 <mbw84@scarletmail.rutgers.edu>
2021-03-31 20:03:23 -0400

    Coffee close window

--- src/sample/coffeeController.java
+++ src/sample/coffeeController.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 public class coffeeController {
+    @FXML
+    public Button returnBTN;
+    
+    public void closeWindow(ActionEvent event){
+        Stage closing = (Stage)returnBTN.getScene().getWindow();
+        closing.close();
+    }
 }
