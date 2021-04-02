77cf31aad7b7b7e167771992d280385f4dc5be9b
mbw84 <mbw84@scarletmail.rutgers.edu>
2021-04-02 11:30:15 -0400

    Actual code
    These got deselected, apologies.

--- src/sample/orderController.java
+++ src/sample/orderController.java
@@ -3,13 +3,28 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class orderController {
     @FXML
     public Button returnBTN;
+    @FXML
+    public Button removeBTN;
+    @FXML
+    public Button placeBTN;
+    @FXML
+    public ListView orderListView;
 
     Controller main;
+
+    public void removeItem(ActionEvent event){
+        
+    }
+
+    public void placeOrder(ActionEvent event){
+
+    }
 
     public void closeWindow(ActionEvent event){
         Stage closing = (Stage)returnBTN.getScene().getWindow();
@@ -18,6 +33,15 @@ public class orderController {
 
     public void setMainController(Controller controller){
         main = controller;
+        displayOrder();
+    }
+
+    public void displayOrder(){
+        MenuItem current[] = main.currentOrder.getItems();
+        for(int i = 0; i < current.length; i++){
+            if(current[i] != null)
+                orderListView.getItems().add(current[i]);
+        }
     }
 
 }
