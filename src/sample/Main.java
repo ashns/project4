2c44985d90b69434df6769eb94f7f12a7799efec
ashns <35615496+ashns@users.noreply.github.com>
2021-04-03 14:39:03 -0400

    comments for main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,22 @@
 package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This class is the main class which runs the UI.
+ * @author Ashley Stankovits, Matthew Walker
+ */
 public class Main extends Application {
 
+    /**
+     * Start initializes the main UI which makes it appear
+     * on the client screen upon running the program.
+     * @param primaryStage which is the UI stage
+     * @throws Exception in case the scene is not able to be located
+     */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -16,6 +25,10 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    /**
+     * This method initializes input.
+     * @param args which is the user input
+     */
     public static void main(String[] args) {
         launch(args);
     }
